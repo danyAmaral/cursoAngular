e1517196ae33213b61ac25611987e3e62453da0b
Danielle Amaral Constâncio e Silva <danielle.silva@prosperi.local>
2019-11-21 12:25:25 -0300

    iniciando arrays

---
+++ array/array.js
@@ -0,0 +1,10 @@
+let aprovados = ['dani', 'jorge', 'carlos']
+aprovados.splice(1,0,'Elemento 1', 'Elemento 2')
+
+console.log(aprovados)
+
+
+let aprovados2 = ['dani', 'jorge', 'carlos']
+aprovados2.splice(1,1,'Elemento 1', 'Elemento 2')
+
+console.log(aprovados2)
