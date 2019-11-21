0f7720d46bc54833846bcfad7c85a2996f5e5c0b
Danielle Amaral Constâncio e Silva <danielle.silva@prosperi.local>
2019-11-21 12:28:00 -0300

    alteração

--- array/array.js
+++ array/array.js
@@ -1,6 +1,8 @@
 let aprovados = ['dani', 'jorge', 'carlos']
-aprovados.splice(1,0,'Elemento 1', 'Elemento 2')
 
+delete aprovados[1]
+
+aprovados.sort()
 console.log(aprovados)
 
 
@@ -8,3 +10,9 @@ let aprovados2 = ['dani', 'jorge', 'carlos']
 aprovados2.splice(1,1,'Elemento 1', 'Elemento 2')
 
 console.log(aprovados2)
+
+
+let aprovados3 = ['dani', 'jorge', 'carlos']
+aprovados3.splice(1,0,'Elemento 1', 'Elemento 2')
+
+console.log(aprovados3)
