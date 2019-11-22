70756a77521e15eb159651513fb1c830553d5b7b
Danielle Amaral Constâncio e Silva <danielle.silva@prosperi.local>
2019-11-22 12:29:07 -0300

    primeiras aulas array

---
+++ array/arrayMetodos.js
@@ -0,0 +1,33 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkomen', 'Massa']
+
+pilotos.pop(); // Remove ultimo elemento do array
+console.log(pilotos)
+
+pilotos.push("Verstappen")
+
+console.log(pilotos)
+
+pilotos.shift(); // Remove o primeiro
+
+console.log(pilotos)
+
+pilotos.unshift("Hamilton") // Add na primeira posição
+console.log(pilotos)
+
+//splice pode add e remover
+
+//adicionar
+
+pilotos.splice(2,0,'Bottas','Massa')
+console.log(pilotos)
+
+//remover
+pilotos.splice(3,1)
+console.log(pilotos)
+
+
+const algunsPilotos1 = pilotos.slice(2) // gera um novo array 
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1,4)
+console.log(algunsPilotos2)
