0f6450c1b6474462e6f76665cac93de2f4521056
danyAmaral <danyamaral27@gmail.com>
2020-03-02 14:44:29 -0300

    seção ajax

--- ajax/server.js
+++ ajax/server.js
@@ -26,6 +26,25 @@ app.post('/upload', (req, res) => {
 
         res.end('Concluído com sucesso');
     })
+});
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    });
+});
+
+
+app.get('/parOuImpar', (req, res) => {
+    //req.body
+    //req.query
+    //req.params
+   const par = parseInt(req.query.numero) % 2 === 0;
+   res.send({
+       resultado: par ? 'par' : 'impar'
+   })
 })
+
 
 app.listen(3033, () => console.log('Executando....'));
