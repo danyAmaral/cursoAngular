79da505d8cdda5c848b4a0ae82245f22af823af8
danyAmaral <danyamaral27@gmail.com>
2020-03-04 19:40:02 -0300

    Finalizando seção WebPack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,28 @@
+const modoDev = process.env.NODE_ENV !== 'production';
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    devServer : {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -26,7 +42,10 @@ module.exports = {
                 'css-loader',
                 'sass-loader',
               ],
-            },
+            },{
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader']
+            }
         ],
-    }
+    }, 
 }
