4e3725f88362b2bd4a3b667fb664173c6ba80e92
danyAmaral <danyamaral27@gmail.com>
2020-03-22 12:12:39 -0300

    continuando seção react

--- react/crud/frontend/src/components/user/UserCrud.jsx
+++ react/crud/frontend/src/components/user/UserCrud.jsx
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Main from '../template/Main';
-headerProps = {
+const headerProps = {
     icon: 'users',
     title: 'Usuários',
-    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
+    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
+}
+
+const baseURL = 'http://localhost:3001/users';
+const initialState = {
+    user: { name: '', email: ''},
+    list: []
 }
 
 export default class UserCrud extends Component {
-    render () {
-        <Main {...headerProps}>
-            Cadastro de Usuário
-        </Main>
+    state = { ...initialState};
+
+    clear() {
+        this.setState({user: initialState.user});
+    }
+
+    save() {
+        const user = this.state.user;
+        const method = user.id ? 'put' : 'post';
+        const url = user.id ? `${baseURL}/${user.id}` : baseURL;
+        axios[method](url, user).then(resp => {
+            const list = this.getUpdatedList(resp.data);
+            this.setState({ user: initialState.user, list})
+        })
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id);
+        list.unshift(user);
+        return list;
+    }
+
+    updateField(event) {
+       // const user = {...this.state.}
+    }
+
+     render () {
+        return (
+            <Main {...headerProps}>
+                Cadastro de Usuário
+             </Main>
+        )
     }
 }
